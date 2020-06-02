3e773388ad6acfa2f7d3c363474302ce1769fb02
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-02 18:13:21 +0530

    Refactor methods and Refactor tests

--- src/main/java/com/org/jenkins/Custom/Jenkins/Distribution/Service/generators/PackageConfigGenerator.java
+++ src/main/java/com/org/jenkins/Custom/Jenkins/Distribution/Service/generators/PackageConfigGenerator.java
@@ -1,51 +1,128 @@
 package com.org.jenkins.Custom.Jenkins.Distribution.Service.generators;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.org.jenkins.Custom.Jenkins.Distribution.Service.Util.Util;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Iterator;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class PackageConfigGenerator {
 
+    static Util util = new Util();
 
-    public static String generatePackageConfig(JSONObject formData) throws JSONException {
+    public static String generatePackageConfig(JSONObject formData)
+        throws  Exception {
 
         //Get the bundle object
         JSONObject bundle = formData.getJSONObject("bundle");
         //Building the initial bundle
+        JSONObject bundleInfo = generateBundle(bundle);
+
+        //Get the build settings object
+        JSONObject buildSettings = formData.getJSONObject("buildSettings");
+        JSONObject docker = generateBuildSettings(buildSettings);
+
+        //Get the war object
+        JSONObject war = formData.getJSONObject("war");
+        //Building the WAR File
+        JSONObject warInfo = generateWarSettings(war);
+
+        //Building system Settings
+        JSONObject systemSettings = formData.getJSONObject("sysSettings");
+        JSONObject sysInfo = generateSystemSettings(systemSettings);
+
+        JSONArray pluginArray = formData.getJSONArray("plugins");
+        JSONArray pluginInfoArray = generatePluginList(pluginArray);
+
+        JSONObject packageConfig = new JSONObject();
+        packageConfig.put("buildSettings", docker);
+        packageConfig.put("bundle", bundleInfo);
+        packageConfig.put("war", warInfo);
+        packageConfig.put("plugins", pluginInfoArray);
+
+        if(formData.has("casc")) {
+            packageConfig.put("casc", generateJCasC());
+        }
+
+        // parse JSON
+        JsonNode jsonNodeTree = new ObjectMapper().readTree(packageConfig.toString());
+        // save it as YAML
+        String jsonAsYaml = new YAMLMapper().writeValueAsString(jsonNodeTree);
+        return jsonAsYaml;
+    }
+
+    private static JSONObject generateBundle(JSONObject bundle) {
         JSONObject bundleInfo = new JSONObject();
         bundleInfo.put("groupId", "io.jenkins.tools.custom-distribution-service.build");
         bundleInfo.put("artifactId", bundle.getString("artifactId"));
         bundleInfo.put("vendor", "Jenkins project");
         bundleInfo.put("title", bundle.getString("title"));
         bundleInfo.put("description", bundle.getString("desc"));
+        return bundleInfo;
+    }
 
-        //Get the build settings object
-        JSONObject buildSettings = formData.getJSONObject("buildSettings");
+    private static JSONObject generateBuildSettings(JSONObject buildSettings) {
         JSONObject dockerInfo = new JSONObject();
         dockerInfo.put("base", buildSettings.getString("base"));
-        if(buildSettings.has("tag")) {
+        if (buildSettings.has("tag")) {
             dockerInfo.put("tag", buildSettings.getString("tag"));
             dockerInfo.put("build", "true");
         }
         JSONObject docker = new JSONObject().put("docker", dockerInfo);
+        return docker;
+    }
 
-        //Get the war object
-        JSONObject war = formData.getJSONObject("war");
-        //Building the WAR File
+    private static JSONObject generateWarSettings(JSONObject warSettings) {
         JSONObject warInfo = new JSONObject();
         warInfo.put("groupId", "org.jenkins-ci.main");
         warInfo.put("artifactId", "jenkins-war");
-        warInfo.put("source", new JSONObject().put("version", war.getString("jenkinsVersion")));
+        warInfo.put("source", new JSONObject().put("version", warSettings.getString("jenkinsVersion")));
+        return warInfo;
+    }
 
-        JSONObject packageConfig = new JSONObject();
-        packageConfig.put("buildSettings", docker);
-        packageConfig.put("bundle", bundleInfo);
-        packageConfig.put("war", warInfo);
+    private static JSONObject generateSystemSettings(JSONObject systemSettings) {
+        JSONObject sysInfo = new JSONObject();
+        sysInfo.put("jenkins.install.runSetupWizard", systemSettings.getString("setupWizard"));
+        sysInfo.put("jenkins.model.Jenkins.slaveAgentPort",
+            systemSettings.getString("slaveAgentPort"));
+        sysInfo.put("jenkins.model.Jenkins.slaveAgentPortEnforce",
+            systemSettings.getString("slaveAgentPortEnforce"));
+        return sysInfo;
+    }
 
-        /*
-        * TO-DO
-        * a) Add Plugin List
-        * */
+    private static JSONArray generatePluginList(JSONArray pluginArray) throws Exception {
 
-        return packageConfig.toString();
+        String updateCenterConfig = new String(
+            Files.readAllBytes(util.getFileFromResources("updateCenter.json").toPath()));
+        JSONObject updateCenterJSON = new JSONObject(updateCenterConfig);
+        JSONObject jsonPluginList = updateCenterJSON.getJSONObject("plugins");
+
+        JSONArray pluginInfoArray = new JSONArray();
+        for (int i = 0; i < pluginArray.length(); i++) {
+            JSONObject pluginObject = pluginArray.getJSONObject(i);
+            Iterator<String> pluginNames = pluginObject.keys();
+            while (pluginNames.hasNext()) {
+                JSONObject pluginInfo = new JSONObject();
+                String pluginName = pluginNames.next();
+                pluginInfo.put("groupId", "org.jenkins-ci.plugins");
+                pluginInfo.put("artifactId", pluginName);
+                pluginInfo.put("source", new JSONObject().put("version", jsonPluginList.getJSONObject(pluginName).getString("version")));
+                pluginInfoArray.put(pluginInfo);
+            }
+        }
+        return pluginInfoArray;
+    }
+
+    private static JSONObject generateJCasC() {
+        JSONObject jcascInfo = new JSONObject();
+        jcascInfo.put("id", "casc");
+        jcascInfo.put("source", new JSONObject().put("dir", "casc.yml"));
+        return jcascInfo;
     }
 
 }
--- src/test/java/com/org/jenkins/Custom/Jenkins/Distribution/Service/generators/PackageConfigGeneratorTest.java
+++ src/test/java/com/org/jenkins/Custom/Jenkins/Distribution/Service/generators/PackageConfigGeneratorTest.java
@@ -4,7 +4,9 @@ package com.org.jenkins.Custom.Jenkins.Distribution.Service.generators;
 import com.org.jenkins.Custom.Jenkins.Distribution.Service.Util.Util;
 import java.nio.file.Files;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
 
 import static com.org.jenkins.Custom.Jenkins.Distribution.Service.generators.PackageConfigGenerator.generatePackageConfig;
 
@@ -12,8 +14,16 @@ public class PackageConfigGeneratorTest {
     Util util = new Util();
 
     @Test
-    public void testPackageConfigGeneration() throws Exception {
-        String sampleConfig = new String(Files.readAllBytes(util.getFileFromResources("packagerConfig/simpleConfig.json").toPath()));
-        generatePackageConfig(new JSONObject(sampleConfig));
+    public void testPackageConfigGeneration() {
+
+        try {
+            String sampleConfig = new String(Files.readAllBytes(util.getFileFromResources("packagerConfig/simpleConfig.json").toPath()));
+            String generatedYAML = generatePackageConfig(new JSONObject(sampleConfig));
+            String expectedYAML = new String(Files.readAllBytes(util.getFileFromResources("packagerConfig/simpleConfig.yml").toPath()));
+            Assert.assertEquals(generatedYAML, expectedYAML);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
