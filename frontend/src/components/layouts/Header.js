1befdbdc095a74a7bdcfea4af6b5fc494f0b1749
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-05 19:17:14 +0530

    Added navigation bar

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 export const Header = () => {
     return(
-        <header className="header" data-tesid="header">
-            <nav>
-                <div className="logo">
-                    <img src="/images/logo.png"></img>
-                </div>
-            </nav>
-        </header>
+        <header class="header colored sticky-header" data-scroll="out">
+				<div class="c-container">
+					<div class="nav-items">
+					<a href="">
+					<img class="site-logo" src="frontend/src/images/jenkins_logo.png" alt=""></img>
+					</a>
+					<ul class="main-nav">
+						<li class="nav-item active">
+						<a class="nav-link active" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#">Plugins</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Community Config</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Package Generation</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">BLOGS</a>
+							</li>
+					</ul>
+					</div>
+				</div>
+			</header>
     )
 }
