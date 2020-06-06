2423e565b785e5a35d2c1f8e215cb7512ad8934d
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-06 18:59:38 +0530

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Header } from './components/layouts/Header'
+import Editor from './components/PackageGeneration/Editor'
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Header />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <Header />
+        <Route path="/generatePackage"><Editor /></Route>
+        </header>
+      </div>
+    </Router>
   );
 }
 
