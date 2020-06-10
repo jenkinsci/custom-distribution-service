9561e49d1e861008db8e8e73c1aa5f35b1509065
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-10 11:41:46 +0530

    Cleaup temp directory after test

--- src/main/java/com/org/jenkins/custom/jenkins/distribution/service/util/Util.java
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/util/Util.java
@@ -1,8 +1,11 @@
 package com.org.jenkins.custom.jenkins.distribution.service.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.logging.Level;
+import org.codehaus.plexus.util.FileUtils;
 
 public class Util {
 
@@ -17,9 +20,20 @@ public class Util {
 
     }
 
-
     public String readStringFromFile(String filename) throws Exception {
         return new String(Files.readAllBytes(getFileFromResources(filename).toPath()));
+    }
+
+    public void cleanupTempDirectory() {
+        // Cleanup the temporary directory
+        try {
+            final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+            if (tmpDir.exists()) {
+                FileUtils.deleteDirectory(tmpDir);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/test/java/com/org/jenkins/custom/jenkins/distribution/service/generators/WarGeneratorTest.java
+++ src/test/java/com/org/jenkins/custom/jenkins/distribution/service/generators/WarGeneratorTest.java
@@ -1,12 +1,15 @@
 package com.org.jenkins.custom.jenkins.distribution.service.generators;
 
+import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
 import org.junit.jupiter.api.Test;
 
 public class WarGeneratorTest {
 
+    private static Util util = new Util();
     @Test
     public void testWarGenerationSucceeds() {
         WarGenerator.generateWAR("1.0");
+        util.cleanupTempDirectory();
     }
 
 }
