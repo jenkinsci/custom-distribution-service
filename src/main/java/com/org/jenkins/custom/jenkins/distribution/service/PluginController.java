298cbf4e0e1dfb6050ecc654eecda5e512f57720
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-10 15:01:08 +0530

    Add getPluginList Controller API

---
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/PluginController.java
@@ -0,0 +1,37 @@
+package com.org.jenkins.custom.jenkins.distribution.service;
+
+import com.org.jenkins.custom.jenkins.distribution.service.services.UpdateCenterService;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("api/plugin")
+public class PluginController {
+
+    private final static Logger LOGGER = Logger.getLogger(PluginController.class.getName());
+    private final UpdateCenterService updateCenterService;
+
+    @Autowired
+    public PluginController(UpdateCenterService updateCenterService) {
+        this.updateCenterService = updateCenterService;
+    }
+
+    @GetMapping(path = "/getPluginList")
+    public ResponseEntity<?> getPlugins() {
+        LOGGER.info("Request Received");
+        try {
+            return new ResponseEntity<>(updateCenterService.downloadUpdateCenterJSON().toMap(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/services/UpdateCenterService.java
@@ -0,0 +1,40 @@
+package com.org.jenkins.custom.jenkins.distribution.service.services;
+
+import com.org.jenkins.custom.jenkins.distribution.service.PluginController;
+import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.logging.Logger;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateCenterService {
+
+    private Util util = new Util();
+
+    private final static Logger LOGGER = Logger.getLogger(UpdateCenterService.class.getName());
+    private static final String UPDATE_CENTER_JSON_URL = "https://updates.jenkins.io/current/update-center.actual.json";
+    
+    public JSONObject downloadUpdateCenterJSON() throws Exception {
+        HttpGet get = new HttpGet(UPDATE_CENTER_JSON_URL);
+        LOGGER.info("Executing Request");
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(get)) {
+            String responseString = EntityUtils.toString(response.getEntity());
+           // FileUtils.writeStringToFile(new File("update-center.json"), responseString, Charset.forName("UTF-8"));
+            /*
+            * Instead of writing to a file it would be better to return a json object as of now
+            * and later once we have the caching mechanism in place we could pull it in from there
+            */
+            return util.convertPayloadToJSON(responseString);
+        }
+    }
+}
