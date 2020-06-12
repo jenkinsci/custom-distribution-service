5368ad863b421f1c7398ae040b64362131c5be32
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-12 17:35:32 +0530

    Add Card views for plugins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './components/layouts/Header'
 import Editor from './components/PackageGeneration/Editor'
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import CardLayout from './components/layouts/CardLayout'
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <header className="App-header">
         <Header />
         <Route path="/generatePackage"><Editor /></Route>
+        <Route path="/pluginList"><CardLayout /></Route>
         </header>
       </div>
     </Router>
---
+++ frontend/src/components/layouts/CardLayout.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import PluginCard from '../PluginCards/pluginCard'
+import { Container, Row, Col } from 'reactstrap';
+
+
+class CardLayout extends React.Component {
+
+    state = {
+        people: [
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "USeful pLugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            },
+            {
+                pluginName: "github Plugin",
+                artifactId: "github-api-plugin",
+                version: "2.3",
+                description: "Useful Github plugin"
+            }
+        ]
+    }
+    render () {
+
+        let pluginCards = this.state.people.map(plugin => {
+            return(
+                <Col sm="3">
+                    <PluginCard plugin = {plugin} />
+                </Col>
+            )
+        })
+        return (
+            <Container fluid>
+                <Row>
+                   {pluginCards} 
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default CardLayout;
