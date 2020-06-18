4e493f57a8df6ef73ea155a45555ae99bc78dc2e
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-18 18:59:45 +0530

    Add pagination using library

--- frontend/src/components/layouts/CardLayout.js
+++ frontend/src/components/layouts/CardLayout.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import PluginCard from '../PluginCards/pluginCard'
 import { Container, Row, Col } from 'reactstrap';
-import Pagination from '../Pagination';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
 
 
 class CardLayout extends React.Component {
 
     state = {
         plugins: {},
-        currentPage : 1,
-        pluginsperPage : 12
+        pluginsperPage : 12,
+        current :1
     }
 
     async componentDidMount() {
@@ -22,33 +23,62 @@ class CardLayout extends React.Component {
 
     render () {
 
-
         let pluginArray = []
         Object.keys(this.state.plugins).map(key => {
             pluginArray.push(this.state.plugins[key])
         });
-        
-        // Get current posts
-        const indexOfLastPlugin =  this.state.currentPage * this.state.pluginsperPage;
+
+          // Get current posts
+        const indexOfLastPlugin =  this.state.current * this.state.pluginsperPage;
         const indexOfFirstPlugin =  indexOfLastPlugin - this.state.pluginsperPage;
-        const currentPlugins = pluginArray.slice(indexOfFirstPlugin, indexOfLastPlugin)
+        const currentPlugins = pluginArray.slice(indexOfFirstPlugin, indexOfLastPlugin)  
+        
+        const updatePageWrapper = (num) => {
+            return (e) => {
+                e.preventDefault();
+                this.setState({current: num});     
+            };
+        };
 
         let pluginCards = currentPlugins.map(plugin => {
-                return(
-                    <Col sm="3">
-                        <PluginCard plugin = {plugin} /> 
-                    </Col>
-                )
+            return(
+                <Col sm="3">
+                    <PluginCard plugin = {plugin} /> 
+                </Col>
+            )
         });
-
-        
 
         return (
             <Container fluid>
                 <Row>
                    {pluginCards} 
                 </Row>
-                <Pagination pluginsPerPage={this.state.pluginsperPage} totalPlugins={pluginArray.length}/>
+          
+                <Pagination aria-label="Page navigation example">
+              {this.state.current !== 1 && <PaginationItem>
+                  <PaginationLink first onClick={updatePageWrapper(1)} />
+              </PaginationItem>}
+              {this.state.current > 1 && <PaginationItem>
+                  <PaginationLink previous onClick ={updatePageWrapper(this.state.current - 1)} />
+              </PaginationItem>}
+              {currentPlugins.map((key, index) => {
+                  const isCurrent = this.state.current == index;
+                  return (
+                      <PaginationItem key={index} active={isCurrent}>
+                          <PaginationLink onClick={updatePageWrapper(index)}>
+                              {index}
+                          </PaginationLink>
+                      </PaginationItem>
+                  );
+              })}
+              {this.state.current !== 100 && <PaginationItem>
+                  <PaginationLink next onClick={updatePageWrapper(this.state.current + 1)} />
+              </PaginationItem>}
+              {this.state.current !== 100 && <PaginationItem>
+                  <PaginationLink last onClick={updatePageWrapper(pluginArray.length)}/>
+              </PaginationItem>}
+          </Pagination>
+
             </Container>
         )
     }
