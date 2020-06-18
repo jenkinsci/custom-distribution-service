faa925de23998342afbb5cba74f7a9f57fe6c762
Sladyn Nunes <sladynnunes98@gmail.com>
2020-06-18 19:32:56 +0530

    Delete extra updateCenterService

--- src/main/java/com/org/jenkins/custom/jenkins/distribution/service/services/UpdateCenterService.java
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/services/UpdateCenterService.java
@@ -1,17 +1,13 @@
 package com.org.jenkins.custom.jenkins.distribution.service.services;
 
-import com.org.jenkins.custom.jenkins.distribution.service.PluginController;
 import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.logging.Logger;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.commons.io.FileUtils;
+
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
