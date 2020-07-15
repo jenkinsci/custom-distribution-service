4db7c15c99ddc486ad960e7b8acfd8a92132ea31
Sladyn <sladynnunes98@gmail.com>
2020-07-15 21:23:03 +0530

    Add packager-download service (#58)
    * Add packager-download service
    
    * Trigger Build
    
    * Applied suggestions
    
    * add javadoc
    
    * Fixed test
    
    * Added suggestions
    
    * Applied commit suggestions
    
    * Fixed download button
    
    * Added suggestions and fixed download button

--- frontend/src/components/PackageGeneration/Editor.js
+++ frontend/src/components/PackageGeneration/Editor.js
@@ -13,19 +13,44 @@ const code = `a {
     margin:10px;
 }`;
 
+// Creates a href element and allows the blob that is passed as data to be saved to the local disk
+function saveData(blob, fileName) {
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  var url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
 class editor extends React.Component {
 
    state = { 
-     code,
+     code:'',
      title: '',
      description: '',
      isLoading: true
-     }
+    }
+
    componentDidMount() {
     this.setState({code: localStorage.getItem("packageConfigYAML")})
     this.setState({title: JSON.parse(localStorage.getItem("packageConfigJSON"))["bundle"]["title"]})
     this.setState({description: JSON.parse(localStorage.getItem("packageConfigJSON"))["bundle"]["desc"]})
    }
+
+   downloadPackagerConfig() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://localhost:8080/package/downloadPackageConfiguration', true);
+    xhr.responseType = "blob";
+    xhr.onload = function () {
+        saveData(this.response, 'casc.yml');
+    };
+    xhr.send(localStorage.getItem("packageConfigYAML"));
+   }
+
+   
 
    render()  {
     const packageJSON = JSON.parse(localStorage.getItem("packageConfigJSON"))
@@ -34,7 +59,10 @@ class editor extends React.Component {
        <div className="row" style = {{padding:"10px", borderRadius:"10px", margin:"0 auto"}}>
         <Editor className="Editor"
         value={this.state.code}
-        onValueChange={code => this.setState({ code })}
+        onValueChange={code => {
+          this.setState({code: code})
+          localStorage.setItem("packageConfigYAML", code)
+        }}
         highlight={code => highlight(code, languages.js)}
         padding={10}
         style={{
@@ -44,7 +72,7 @@ class editor extends React.Component {
         }}
       />
       <div className="column">
-      <Button style = {{backgroundColor:"#185ecc", fontSize:"25px", margin:"10px"}} >Download Packager Config </Button>
+      <Button onClick = { this.downloadPackagerConfig }style = {{backgroundColor:"#185ecc", fontSize:"25px", margin:"10px"}} >Download Packager Config </Button>
       <Button style = {{backgroundColor:"#185ecc", fontSize:"25px"}} >Download War File </Button>
       <Card style = {{ margin:"10px"}}>
         <CardBody>
