03aaa160f372f3169730641eb446a4aae01cd439
Sladyn <sladynnunes98@gmail.com>
2020-07-21 18:49:28 +0530

    Add War download feature (#68)
    * Add War download feature
    
    * Added code suggestions
    
    * Committed suggestions
    
    * Fixed arguments
    
    * Removed test and added war download funtion to frontend
    
    * Trigger Build

--- src/main/java/com/org/jenkins/custom/jenkins/distribution/service/PackagerController.java
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/PackagerController.java
@@ -1,10 +1,10 @@
 package com.org.jenkins.custom.jenkins.distribution.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.org.jenkins.custom.jenkins.distribution.service.services.PackagerDownloadService;
 import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
+import java.util.Map;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.util.logging.Logger;
 import org.json.JSONObject;
 import org.springframework.core.io.InputStreamResource;
@@ -15,16 +15,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.Yaml;
 
 import static com.org.jenkins.custom.jenkins.distribution.service.generators.PackageConfigGenerator.generatePackageConfig;
-
 @RestController
 @CrossOrigin
 @RequestMapping("/package")
 
 public class PackagerController {
 
-    private Util util = new Util();
+    private static Util util = new Util();
+
     private final static Logger LOGGER = Logger.getLogger(PackagerController.class.getName());
 
     /**
@@ -52,6 +53,24 @@ public class PackagerController {
     }
 
     /**
+     * Takes the configuration in the browsers editor and generates and returns the 
+     * war file.
+     * 
+     * @param postPayload The content of the text editor in the browser 
+     * @return a ResponseEntity instance with a body containing the generated jenkins.war package.
+     */
+    @PostMapping (path = "/downloadWarPackage")
+    public ResponseEntity<?> downloadWAR(@RequestBody String postPayload) {
+        LOGGER.info("Request Received for downloading war file with configuration" + postPayload);
+        try {
+            return new PackagerDownloadService().downloadWAR(getWarVersion(), postPayload);
+        } catch (Exception e) {
+            LOGGER.severe(e.toString());
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
      * Send the content of the browser's text editor to the client (a download).
      *
      * @param postPayload The content of the text editor in the browser.
@@ -71,4 +90,10 @@ public class PackagerController {
         }
     }
 
+    private String getWarVersion() throws Exception {
+        Yaml yaml = new Yaml();
+        Map<String , Map<String,String>> yamlMaps = (Map<String, Map<String,String>>) yaml.load(util.readStringFromFile("packager-config.yml"));
+        JSONObject json = new JSONObject(new ObjectMapper().writeValueAsString(yamlMaps.get("war")));
+        return json.getJSONObject("source").get("version").toString();
+    }
 }
--- src/main/java/com/org/jenkins/custom/jenkins/distribution/service/generators/WarGenerator.java
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/generators/WarGenerator.java
@@ -1,35 +1,33 @@
 package com.org.jenkins.custom.jenkins.distribution.service.generators;
 
-import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
 import io.jenkins.tools.warpackager.lib.config.Config;
 import io.jenkins.tools.warpackager.lib.impl.Builder;
-
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class WarGenerator {
 
-    private static Util util = new Util();
-
-    private static final String PACKAGER_CONFIG_YAML = "packager-config.yml";
+    private final static Logger LOGGER = Logger.getLogger(WarGenerator.class.getName());
     private static final String TEMP_PREFIX = "CDS";
 
-    public static void generateWAR(String versionName) {
+
+    public static File generateWAR(String versionName, String configuration) throws Exception {
+        LOGGER.info("Generating War File");
         final Config cfg;
-        Path tempDirWithPrefix = null;
-        try {
-            tempDirWithPrefix = Files.createTempDirectory(TEMP_PREFIX);
-            final File configPath = util.getFileFromResources(PACKAGER_CONFIG_YAML);
-            cfg = Config.loadConfig(configPath);
+        Path tempDirWithPrefix = Files.createTempDirectory(TEMP_PREFIX);
+            File packagerConfigFile = File.createTempFile("packager-config", ".yml");
+            byte[] buf = configuration.getBytes();
+            Files.write(packagerConfigFile.toPath(), buf);
+            cfg = Config.loadConfig(packagerConfigFile);
             cfg.buildSettings.setTmpDir(tempDirWithPrefix.toFile());
             cfg.buildSettings.setVersion(versionName);
             cfg.buildSettings.setInstallArtifacts(true);
             new Builder(cfg).build();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            util.cleanupTempDirectory(tempDirWithPrefix.toFile());
-        }
+            LOGGER.info("Cleaning up temporary directory");
+            packagerConfigFile.deleteOnExit();
+            return cfg.getOutputWar();
     }
+
 }
