26790bb6796d1ad42155eb3061478b97c99f578f
Sladyn <sladynnunes98@gmail.com>
2020-07-23 20:02:27 +0530

    Add search functionality (#84)
    * Add build instuctions without dockerfile
    
    * Add search functionality for searching community configs
    
    * Remove logging statement

--- frontend/src/components/CommunityConfiguration/communityConfigLayout.js
+++ frontend/src/components/CommunityConfiguration/communityConfigLayout.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PluginCard from '../PluginCards/pluginCard'
 import { Container, Row, Col, Button} from 'reactstrap';
-import { Pagination, PaginationItem, PaginationLink, Spinner } from 'reactstrap';
 import ConfigurationCard from './configurationCard';
+import { InputGroup, InputGroupAddon,Input, Label } from 'reactstrap';
+
 
 class communityConfigLayout extends React.Component {
 
@@ -17,19 +18,57 @@ class communityConfigLayout extends React.Component {
         const body = await response.json();
         this.setState({data: body})
     }
+    
+    onchange = pluginName => {
+        console.log("Searching Plugins")
+        this.setState({search: pluginName.target.value})
+    }
 
     render() {
+        const search  = this.state.search
         let configurationCards;
-        configurationCards = this.state.data.map(config => {
-            return(
-                <Col sm="3">
-                    <ConfigurationCard config = {config} /> 
-                </Col>
-            )
-        }) 
+        if (search !== "") {
+            configurationCards = this.state.data.map(config => {
+                if(!(config["name"].toLowerCase().indexOf( search.toLowerCase() ) === -1)) {
+                    return(
+                    <Col sm="3">
+                        <ConfigurationCard config = {config} /> 
+                    </Col>
+                    )
+                }
+            })    
+        } else {
+            configurationCards = this.state.data.map(config => {
+                if (search !== "" && config["name"].toLowerCase().indexOf( search.toLowerCase() ) === -1) {
+                    return null
+                }
+                return(
+                    <Col sm="3">
+                        <ConfigurationCard config = {config} /> 
+                    </Col>
+                )
+            })  
+        }   
 
         return(
-            <Container fluid style = {{height: "100vh"}}>
+            <Container fluid style = {{height: "100vh"}} class="column">
+            {/* Div to center align the search box */}
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }} >
+
+
+            {/* Search Bar */}
+            <InputGroup style={{margin:"10px", width:"40%"}}>
+            <Input onChange = {this.onchange} />
+            <InputGroupAddon addonType="append">
+            <Button  style = {{backgroundColor:"#185ecc"}} >Search Community Config</Button>
+            </InputGroupAddon>
+            </InputGroup>
+            </div>
             <Row>
                 {configurationCards} 
              </Row>
