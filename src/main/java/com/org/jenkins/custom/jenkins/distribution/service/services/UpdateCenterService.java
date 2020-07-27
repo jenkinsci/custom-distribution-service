425221108b5dd5ec6d43796c16c3abf456c93533
Sladyn Nunes <sladynnunes98@gmail.com>
2020-07-27 12:59:27 +0530

    Fix error message and general cleaup

--- src/main/java/com/org/jenkins/custom/jenkins/distribution/service/services/UpdateCenterService.java
+++ src/main/java/com/org/jenkins/custom/jenkins/distribution/service/services/UpdateCenterService.java
@@ -28,6 +28,25 @@ public class UpdateCenterService {
         * Instead of writing to a file it would be better to return a json object as of now
         * and later once we have the caching mechanism in place we could pull it in from there
         */
+
+        LOGGER.info("Update flag is " + updateFlag);
+        LOGGER.info("Update Center Path " + updateCenterFilePath);
+
+        if(updateFlag == 0) {
+            File updateCenterFile = File.createTempFile("update-center", ".json");
+            updateCenterFilePath = updateCenterFile.getPath();
+            LOGGER.info("Creating a new file" + updateCenterFile.getPath());
+            HttpGet get = new HttpGet(UPDATE_CENTER_JSON_URL);
+            LOGGER.info("Executing Request at " + UPDATE_CENTER_JSON_URL);
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(get);
+            responseString = EntityUtils.toString(response.getEntity());
+            byte[] buf = responseString.getBytes();
+            Files.write(updateCenterFile.toPath(), buf);
+            updateFlag = 1;
+        } else {
+            responseString = readFileAsString(updateCenterFilePath);
+        }
         LOGGER.info("Returning Response");
         return util.convertPayloadToJSON(responseString);
     }
