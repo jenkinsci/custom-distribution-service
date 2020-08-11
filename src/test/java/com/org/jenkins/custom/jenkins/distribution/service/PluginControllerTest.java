aea1ed9a377c19e6d7b01e5830f5cb93e7eaf6d5
Sladyn Nunes <sladynnunes98@gmail.com>
2020-08-11 13:13:30 +0530

    Add more tests

--- src/test/java/com/org/jenkins/custom/jenkins/distribution/service/PluginControllerTest.java
+++ src/test/java/com/org/jenkins/custom/jenkins/distribution/service/PluginControllerTest.java
@@ -1,5 +1,7 @@
 package com.org.jenkins.custom.jenkins.distribution.service;
 
+import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
+import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +10,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class PluginControllerTest extends SpringMVCSetup {
+
+    private Util util = new Util();
 
     @Override
     @Before
@@ -19,10 +24,25 @@ public class PluginControllerTest extends SpringMVCSetup {
     }
 
     @Test
-    public void getPluginList() throws Exception {
+    public void return_status_200() throws Exception {
+
+        // Setup Mock
+        String dummyUpdateBody = util.readStringFromFile("updateCenter.json");
+        when(updateService.downloadUpdateCenterJSON()).thenReturn(util.convertPayloadToJSON(dummyUpdateBody));
+
         String uri = "/api/plugin/getPluginList";
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
+    }
+
+    @Test
+    public void return_status_404() throws Exception {
+        // Mock the update Center service class to throw an Exception
+        when(updateService.downloadUpdateCenterJSON()).thenThrow(IOException.class);
+        String uri = "/api/plugin/getPluginList";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(404, status);
     }
 }
--- src/test/java/com/org/jenkins/custom/jenkins/distribution/service/SpringMVCSetup.java
+++ src/test/java/com/org/jenkins/custom/jenkins/distribution/service/SpringMVCSetup.java
@@ -1,9 +1,11 @@
 package com.org.jenkins.custom.jenkins.distribution.service;
 
+import com.org.jenkins.custom.jenkins.distribution.service.services.UpdateCenterService;
 import java.io.IOException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,12 +23,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public abstract class SpringMVCSetup {
 
     protected MockMvc mvc;
+
     @Autowired
     WebApplicationContext webApplicationContext;
+
+    @MockBean
+    public UpdateCenterService updateService;
 
     protected void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
     protected String mapToJson(Object obj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(obj);
