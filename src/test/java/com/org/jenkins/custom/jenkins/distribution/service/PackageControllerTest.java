096fdd5361d8c539e28c4e0ca1300024dbdd584d
Sladyn Nunes <sladynnunes98@gmail.com>
2020-08-15 14:01:37 +0530

    Added more tests

--- src/test/java/com/org/jenkins/custom/jenkins/distribution/service/PackageControllerTest.java
+++ src/test/java/com/org/jenkins/custom/jenkins/distribution/service/PackageControllerTest.java
@@ -2,13 +2,21 @@ package com.org.jenkins.custom.jenkins.distribution.service;
 
 import com.org.jenkins.custom.jenkins.distribution.service.util.Util;
 import java.nio.charset.Charset;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
-
 public class PackageControllerTest extends SpringMVCSetup{
+
+    @Override
+    @Before
+    public void setup() {
+        super.setup();
+    }
 
     private Util util = new Util();
 
@@ -16,12 +24,33 @@ public class PackageControllerTest extends SpringMVCSetup{
         .forName("utf8"));
 
     @Test
-    public void return_package_configuration() throws Exception {
+    public void generate_packager_configYAML() throws Exception {
 
-        // Setup Mock
         String uri = "/package/getPackageConfiguration";
-        mvc.perform( MockMvcRequestBuilders.post(uri)
+        MvcResult result = mvc.perform( MockMvcRequestBuilders.post(uri)
             .contentType(APPLICATION_JSON_UTF8)
-            .content(util.readStringFromFile("simple-config.json")));
+            .content(util.readStringFromFile("simple-config.json"))).andReturn();
+        Assert.assertEquals(result.getResponse().getContentAsString(), util.readStringFromFile("simple-config.yaml"));
+    }
+
+    @Test
+    public void download_packager_configYAML() throws Exception {
+        String uri = "/package/downloadPackageConfiguration";
+        MvcResult result = mvc.perform( MockMvcRequestBuilders.post(uri)
+            .contentType(APPLICATION_JSON_UTF8)
+            .content(util.readStringFromFile("simple-config.yaml"))).andReturn();
+        Assert.assertEquals(result.getResponse().getContentAsString(), util.readStringFromFile("simple-config.yaml"));
+    }
+
+    @Test
+    public void fail_warDownload_due_to_casc_section() {
+        String uri = "/package/downloadWarPackage";
+        try {
+             mvc.perform( MockMvcRequestBuilders.post(uri)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(util.readStringFromFile("simple-config.yaml"))).andReturn();
+        } catch ( Exception e) {
+            Assert.fail();
+        }
     }
 }
