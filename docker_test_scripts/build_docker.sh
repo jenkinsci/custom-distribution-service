1495a6dba61e02267aa142327af98fa1c9f2fad1
Sladyn Nunes <sladynnunes98@gmail.com>
2020-08-21 19:45:03 +0530

    Add testing scritps

--- docker_test_scripts/build_docker.sh
+++ docker_test_scripts/build_docker.sh
@@ -2,3 +2,8 @@
 if docker-compose up -d --build; then
    echo "Command Returned an error"
    exit 1 
+fi
+
+if docker-compose up; then
+   exit 1
+fi
--- run_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if docker-compose up; then
-   echo "Command Returned an error"
-   exit 1 
